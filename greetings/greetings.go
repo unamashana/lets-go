1cd65c235b931d18aa291305d38ddef2ebc895d0
Hana Mohan <unamashana@gmail.com>
2022-10-21 20:29:22 +0200

    learning go

---
+++ greetings/greetings.go
@@ -0,0 +1,17 @@
+package greetings
+
+import (
+	"fmt"
+	"errors"
+)
+
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v, Welcome!", name)
+	return message, nil
+}
